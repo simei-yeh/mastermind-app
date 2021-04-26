e6158e1f639aa6f5bb0d34b5b0aa308550d9fcdc
simei-yeh <simei.yeh@gmail.com>
2021-04-26 09:58:41 -0700

    added axios and created random number generator API

--- src/components/randomNumberGeneratorAPI.js
+++ src/components/randomNumberGeneratorAPI.js
@@ -1,2 +1,42 @@
 const axios = require('axios');
-const config = require('./config.json');
+
+const baseURL = 'https://www.random.org/integers/';
+
+class randomNumberAPI {
+  findNewNumbers() {
+    return axios.get(baseURL, {
+      params: {
+        num: 4,
+        min: 0,
+        max: 7,
+        col: 1,
+        base: 10,
+        format: 'plain',
+        rnd: 'new'
+      }
+    })
+      .then((response) => {
+        return response.data;
+      })
+      .then((data) => {
+        console.log(data);
+        return data.replace(/[\n]/g,'').split('');
+      })
+      .catch((err) => {
+        if (err.response) {
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else if (err.request) {
+          console.log(err.request)
+        } else {
+          console.log('Error', err.message)
+        }
+      })
+  }
+}
+
+module.exports = new randomNumberAPI();
+
+const randomAPI = new randomNumberAPI();
+randomAPI.findNewNumbers();
